f0771a0335de92888bb1161d3f5eb3f0a10d0e15
mwilson877 <mikewilson877@yahoo.com>
2025-09-18 11:26:29 -0500

    Update msgraph_mailbox_toolkit.py
    Fixed error in get_child_folders function.

--- msgraph_mailbox_toolkit.py
+++ msgraph_mailbox_toolkit.py
@@ -161,7 +161,7 @@ def get_child_folders(mailbox, main_folder_id, access_token):
         "Content-Type": "application/json"
     }
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
 
     data = response.json()
